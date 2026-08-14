bc11cfae8354f6b474c6f4a83cff44f98593421f
Abid-Hussain13 <abidhussainme1@gmail.com>
2026-08-14 10:05:51 +0500

    cleaning the client folder

--- apps/client/src/pages/guest/JoinPage.tsx
+++
@@ -1,6 +0,0 @@
-import { useParams } from 'react-router-dom'
-
-export function JoinPage() {
-  const { token } = useParams<{ token: string }>()
-  return <div className="p-6 font-body text-fg">Join session {token}</div>
-}
